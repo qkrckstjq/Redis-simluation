913055fcede99bad1e4905bc06b30048ff7583bd
qkrckstjq <cksk49@naver.com>
2026-06-19 20:02:35 +0900

    [refact] Update snapshot dto for clustered nearby entity results

--- src/main/java/com/example/world/entity/EntitySnapshotDto.java
+++ src/main/java/com/example/world/entity/EntitySnapshotDto.java
@@ -17,6 +17,7 @@ public class EntitySnapshotDto {
     private int hp;
     private int x;
     private int y;
+    private Long targetId;
     private List<Long> nearbyIds;
 
     public EntitySnapshotDto(Long id, TypeEnum type, StateEnum state, int stamina, int hp, int x, int y) {
@@ -29,7 +30,17 @@ public class EntitySnapshotDto {
         this.y = y;
     }
 
-    public EntitySnapshotDto(Long id, TypeEnum type, StateEnum state, int stamina, int hp, int x, int y, List<Long> nearbyIds) {
+    public EntitySnapshotDto(
+            Long id,
+            TypeEnum type,
+            StateEnum state,
+            int stamina,
+            int hp,
+            int x,
+            int y,
+            List<Long> nearbyIds,
+            Long targetId
+    ) {
         this.id = id;
         this.type = type;
         this.state = state;
@@ -38,5 +49,6 @@ public class EntitySnapshotDto {
         this.x = x;
         this.y = y;
         this.nearbyIds = nearbyIds;
+        this.targetId = targetId;
     }
 }
