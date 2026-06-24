9d16470e77e83e7cb55dbf48c0e24dfcc41acee3
qkrckstjq <cksk49@naver.com>
2026-06-24 20:14:38 +0900

    [feat] add updateEntity Lua Script

--- src/main/resources/lua/update_entity.lua
+++ src/main/resources/lua/update_entity.lua
@@ -1,39 +1,82 @@
-local entityKey = KEYS[1]
-local geoKey = KEYS[2]
-local worldKey = KEYS[3]
+local entityKey      = KEYS[1]
+local prevGeoKey     = KEYS[2]
+local nextGeoKey     = KEYS[3]
+local worldKey       = KEYS[4]
 
-local hp = tonumber(ARGV[1])
-local x = ARGV[2]
-local y = ARGV[3]
-local state = ARGV[4]
-local stamina = ARGV[5]
-local longitude = ARGV[6]
-local latitude = ARGV[7]
-local entityId = ARGV[8]
+local isDead         = ARGV[1]
+local cellChanged    = ARGV[2]
 
-if hp <= 0 then
+if isDead == "1" then
+
     redis.call("DEL", entityKey)
-    redis.call("ZREM", geoKey, entityKey)
-    redis.call("SREM", worldKey, entityId)
-    return 0
+
+    redis.call(
+        "ZREM",
+        prevGeoKey,
+        entityKey
+    )
+
+    redis.call(
+        "SREM",
+        worldKey,
+        entityKey
+    )
+
+    return
 end
+
+if cellChanged == "1" then
+
+    redis.call(
+        "DEL",
+        entityKey
+    )
+
+    redis.call(
+        "ZREM",
+        prevGeoKey,
+        entityKey
+    )
+
+    redis.call(
+        "SREM",
+        worldKey,
+        entityKey
+    )
+end
+
+local nextEntityKey = ARGV[3]
 
 redis.call(
     "HSET",
-    entityKey,
-    "hp", hp,
-    "x", x,
-    "y", y,
-    "state", state,
-    "stamina", stamina
+    nextEntityKey,
+
+    "id",ARGV[4],
+    "age",ARGV[5],
+    "type",ARGV[6],
+    "state",ARGV[7],
+    "stamina",ARGV[8],
+    "hp",ARGV[9],
+    "x",ARGV[10],
+    "y",ARGV[11],
+    "breedReady",ARGV[12],
+    "breedReadyTick",ARGV[13],
+    "cellKey",ARGV[14],
+    "targetId",ARGV[15]
 )
 
 redis.call(
     "GEOADD",
-    geoKey,
-    longitude,
-    latitude,
-    entityKey
+    nextGeoKey,
+    ARGV[16],
+    ARGV[17],
+    nextEntityKey
+)
+
+redis.call(
+    "SADD",
+    worldKey,
+    nextEntityKey
 )
 
 return 1
