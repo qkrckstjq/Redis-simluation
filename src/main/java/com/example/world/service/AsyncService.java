90ed77ad03ebec45fc311cd9fcb529f36d4c3e64
qkrckstjq <cksk49@naver.com>
2026-06-25 18:14:27 +0900

    [feat] Converting stream and WebSocket services to asynchronous services

--- src/main/java/com/example/world/service/AsyncService.java
+++ src/main/java/com/example/world/service/AsyncService.java
@@ -1,4 +1,43 @@
 package com.example.world.service;
 
+import com.example.world.entity.EntitySnapshotDto;
+import com.example.world.entity.RedisEntity;
+import com.example.world.entity.Tick;
+import com.example.world.repository.RedisRepository;
+import com.example.world.stream.StreamService;
+import com.example.world.websocket.WebSocketMapper;
+import com.example.world.websocket.WebSocketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AsyncService {
+
+    private final StreamService streamService;
+    private final EventMapper eventMapper;
+    private final RedisRepository redisRepository;
+    private final WebSocketMapper webSocketMapper;
+    private final WebSocketService webSocketService;
+
+    @Async("streamExecutor")
+    public void publish(List<RedisEntity> entityList) {
+
+        redisRepository.requestPipeLine(
+                streamService.publish(
+                        eventMapper.entitiesToEvents(entityList)
+                )
+        );
+    }
+
+    @Async("webSocketExecutor")
+    public void sendSnapshots(List<EntitySnapshotDto> snapshotDtoList) {
+
+        Tick tick = webSocketMapper.redisEntitiesToTick(snapshotDtoList);
+
+        webSocketService.sendSnapShots(tick);
+    }
 }
