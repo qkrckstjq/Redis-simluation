0b2c2deba495c63285be37258de6d863462228e5
qkrckstjq <cksk49@naver.com>
2026-06-26 22:35:58 +0900

    [feat] add skipSearch field to entity mapping

--- src/main/java/com/example/world/service/BehaviorService.java
+++ src/main/java/com/example/world/service/BehaviorService.java
@@ -351,7 +351,8 @@ public class BehaviorService {
                 false,
                 0,
                 null,
-                null
+                null,
+                false
         );
         spawnList.add(child);
 
