ec553b370e98b0e720317e7d641be22c4fec13a1
qkrckstjq <cksk49@naver.com>
2026-06-29 17:31:17 +0900

    [refact] move entity mapping methods to static methods in EntityMapper

--- src/main/java/com/example/world/service/EntityMapper.java
+++ src/main/java/com/example/world/service/EntityMapper.java
@@ -4,10 +4,12 @@ import com.example.world.entity.NextMove;
 import com.example.world.entity.RedisEntity;
 import com.example.world.entity.StateEnum;
 import com.example.world.entity.TypeEnum;
+import com.example.world.util.ByteTypeConverter;
 import org.springframework.data.geo.GeoResults;
 import org.springframework.data.redis.connection.RedisGeoCommands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +36,7 @@ public interface EntityMapper {
 
     public Map<Long, RedisEntity> entitiesToHashMap(List<RedisEntity> entityList);
 
-    public default List<RedisEntity> objectsToRedisEntities(List<Object> objectList) {
+    public static List<RedisEntity> objectsToRedisEntities(List<Object> objectList) {
         List<RedisEntity> entities = new ArrayList<>();
 
         for (Object result : objectList) {
@@ -68,5 +70,152 @@ public interface EntityMapper {
             );
         }
         return entities;
+    }
+
+    public static Map<byte[], byte[]> entityToByteMap(RedisEntity entity) {
+        Map<byte[], byte[]> map = new HashMap<>();
+        map.put(
+                ByteTypeConverter.stringToByte("id"),
+                ByteTypeConverter.numToByte(Math.toIntExact(entity.getId()))
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("age"),
+                ByteTypeConverter.numToByte(entity.getAge())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("hp"),
+                ByteTypeConverter.numToByte(entity.getHp())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("x"),
+                ByteTypeConverter.stringToByte(String.valueOf(entity.getX()))
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("y"),
+                ByteTypeConverter.stringToByte(String.valueOf(entity.getY()))
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("type"),
+                ByteTypeConverter.stringToByte(entity.getType().name())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("state"),
+                ByteTypeConverter.stringToByte(entity.getState().getState())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("stamina"),
+                ByteTypeConverter.numToByte(entity.getStamina())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("breedReady"),
+                ByteTypeConverter.stringToByte(String.valueOf(entity.isBreedReady()))
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("breedReadyTick"),
+                ByteTypeConverter.numToByte(entity.getBreedReadyTick())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("cell"),
+                ByteTypeConverter.stringToByte(entity.getCellKey())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("targetId"),
+                ByteTypeConverter.numToByte(entity.getTargetId())
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("skipSearch"),
+                ByteTypeConverter.numToByte(String.valueOf(entity.isSkipSearch()))
+        );
+        return map;
+    }
+
+    public static Map<byte[], byte[]> newEntityToByteMap(
+            int id,
+            int hp,
+            String type,
+            String state,
+            int stamina,
+            int x,
+            int y,
+            String cellKey
+    ) {
+        Map<byte[], byte[]> map = new HashMap<>();
+        map.put(
+                ByteTypeConverter.stringToByte("id"),
+                ByteTypeConverter.numToByte(id)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("age"),
+                ByteTypeConverter.numToByte(0)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("hp"),
+                ByteTypeConverter.numToByte(hp)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("type"),
+                ByteTypeConverter.stringToByte(type)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("state"),
+                ByteTypeConverter.stringToByte(state)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("stamina"),
+                ByteTypeConverter.numToByte(stamina)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("x"),
+                ByteTypeConverter.numToByte(x)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("y"),
+                ByteTypeConverter.numToByte(y)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("breedReady"),
+                ByteTypeConverter.stringToByte(String.valueOf(false))
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("breedReadyTick"),
+                ByteTypeConverter.numToByte(0)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("cell"),
+                ByteTypeConverter.stringToByte(cellKey)
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("targetId"),
+                ByteTypeConverter.stringToByte("null")
+        );
+
+        map.put(
+                ByteTypeConverter.stringToByte("skipSearch"),
+                ByteTypeConverter.stringToByte(String.valueOf(false))
+        );
+        return map;
     }
 }
---
+++ src/main/java/com/example/world/service/inmemory/InMemoryEntityService.java
@@ -0,0 +1,4 @@
+package com.example.world.service.inmemory;
+
+public class InMemoryEntityService {
+}
