8b238b2ee1883d74bb94f15dca742fd1e6273f9e
qkrckstjq <cksk49@naver.com>
2026-06-30 19:40:34 +0900

    [feat] add BeanConfig for interface-based service management

--- src/main/java/com/example/world/config/BeanConfig.java
+++ src/main/java/com/example/world/config/BeanConfig.java
@@ -1,4 +1,41 @@
 package com.example.world.config;
 
+import com.example.world.cluster.service.EntityClusterMapper;
+import com.example.world.cluster.service.EntityClusterService;
+import com.example.world.service.EntityMapper;
+import com.example.world.service.EntityService;
+import com.example.world.service.RedisEntityService;
+import com.example.world.service.RedisService;
+import com.example.world.service.inmemory.InMemoryEntityService;
+import com.example.world.service.inmemory.InMemoryRedisService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfig {
+
+    @Bean
+    public RedisService redisService(InMemoryRedisService redisService) {
+        return redisService;
+    }
+
+    @Bean
+    public EntityService entityService(InMemoryEntityService entityService) {
+        return entityService;
+    }
+
+    @Bean
+    public EntityMapper entityMapper(EntityClusterMapper entityMapper) {
+        return entityMapper;
+    }
+
+//    @Bean
+//    public RedisService redisService(EntityClusterService redisService) {
+//        return redisService;
+//    }
+//
+//    @Bean
+//    public EntityService entityService(RedisEntityService entityService) {
+//        return entityService;
+//    }
 }
