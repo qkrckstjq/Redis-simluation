7f9db147524fa5d215ebd1daccbaf94776267884
qkrckstjq <cksk49@naver.com>
2026-07-05 20:49:12 +0900

    [feat] add TimedAspect bean for Micrometer

--- src/main/java/com/example/world/config/BeanConfig.java
+++ src/main/java/com/example/world/config/BeanConfig.java
@@ -1,15 +1,15 @@
 package com.example.world.config;
 
 import com.example.world.cluster.service.EntityClusterMapper;
-import com.example.world.cluster.service.EntityClusterService;
 import com.example.world.entity.log.PerformanceLog;
 import com.example.world.entity.log.PerformanceMetric;
 import com.example.world.service.EntityMapper;
 import com.example.world.service.EntityService;
-import com.example.world.service.RedisEntityService;
 import com.example.world.service.RedisService;
 import com.example.world.service.inmemory.InMemoryEntityService;
 import com.example.world.service.inmemory.InMemoryRedisService;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -49,5 +49,10 @@ public class BeanConfig {
     @Bean
     public PerformanceLog performanceLog(PerformanceMetric performanceMetric) {
         return new PerformanceLog(performanceMetric);
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry) {
+        return new TimedAspect(meterRegistry);
     }
 }
