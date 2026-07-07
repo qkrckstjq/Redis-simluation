8867f0c6db1fa3d7a71818a586abdcdc87df858b
qkrckstjq <cksk49@naver.com>
2026-07-07 21:40:20 +0900

    [refact] replace stream with forEach

--- src/main/java/com/example/world/service/EventMapper.java
+++ src/main/java/com/example/world/service/EventMapper.java
@@ -1,6 +1,7 @@
 package com.example.world.service;
 
 import com.example.world.entity.*;
+import com.example.world.util.ByteTypeConverter;
 import com.example.world.websocket.WebSocketMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,16 +22,16 @@ public class EventMapper {
     private final ObjectMapper objectMapper;
 
     public List<SimulationEvent> entitiesToEvents(List<RedisEntity> entityList) {
-        List<SimulationEvent> result = new ArrayList<>();
         Long curTick = webSocketMapper.getTick();
-        entityList.forEach(entity -> {
-            StateEnum entityState = entity.getState();
-            if(entityState.equals(MOVE) || entityState.equals(IDLE) || entityState.equals(REST)) return;
-            long entityId = entity.getId();
-            Map<String, String> payLoad = entityToMap(entity, entityState);
-            result.add(new SimulationEvent(entityState, curTick, entityId, payLoad));
-        });
-        return result;
+        return entityList.stream()
+                .map(entity -> new SimulationEvent(
+                        entity.getState(),
+                        curTick,
+                        entity.getId(),
+                        entity.getTargetId() == null ? 0 : entity.getTargetId(),
+                        entity.getAge()
+                ))
+                .toList();
     }
 
     public String stringToValue(Object object) {
@@ -63,9 +64,39 @@ public class EventMapper {
         Map<String, String> result = new HashMap<>();
         result.computeIfPresent("targetId",
                 (k, targetId) -> targetId.equals("null") ? null : String.valueOf(entity.getTargetId()));
-
         result.put("x", String.valueOf(entity.getX()));
         result.put("y", String.valueOf(entity.getY()));
+        return result;
+    }
+
+    public Map<byte[], byte[]> eventToMap(SimulationEvent event) {
+        Map<byte[], byte[]> result = new HashMap<>();
+
+        result.put(
+                ByteTypeConverter.stringToByte("tick"),
+                ByteTypeConverter.numToByte(event.getTick())
+        );
+
+        result.put(
+                ByteTypeConverter.stringToByte("state"),
+                ByteTypeConverter.stringToByte(event.getState().name())
+        );
+
+        result.put(
+                ByteTypeConverter.stringToByte("entityId"),
+                ByteTypeConverter.numToByte(event.getEntityId())
+        );
+
+        result.put(
+                ByteTypeConverter.stringToByte("targetId"),
+                ByteTypeConverter.numToByte(event.getTargetId())
+        );
+
+        result.put(
+                ByteTypeConverter.stringToByte("age"),
+                ByteTypeConverter.numToByte(event.getAge())
+        );
+
         return result;
     }
 }
